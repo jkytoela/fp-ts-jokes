b8eb5d35019b885f162dafc5dbf5bd385ae3c207
Jaakko Kytölä <kytola.jaakko@gmail.com>
2022-02-01 12:47:27 +0200

    initial commit 🚀

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <blockquote>The man who comes back through the door in the wall will never be quite the same as the man who went out.</blockquote>
+      <cite>Aldous Huxley</cite>
+    </React.Fragment>
+  );
+}
+
+export default App;
